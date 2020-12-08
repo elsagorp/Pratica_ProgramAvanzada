27d7ab151b284aadfb0d4e2395411eabeed924c1
elsagorp <egordillop@alumnos.nebrija.es>
2020-12-08 18:23:32 +0100

    Pratica4

---
+++ ProgramAvanzadaPratica3/src/Pratica4/Tests/ListaHotelsTest.java
@@ -0,0 +1,24 @@
+package Pratica4.Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListaHotelsTest {
+
+	@Test
+	final void testAddto() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testExist() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMaxCost() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
